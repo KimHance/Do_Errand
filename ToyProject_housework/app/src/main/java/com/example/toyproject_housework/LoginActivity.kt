c38e7eac9d5354ca42d6dc0005bd240043c36bf7
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-02-06 00:46:56 +0900

    회원등록 , 로그인 1차 구현
    firebase auth, firestore 이용

--- ToyProject_housework/app/src/main/java/com/example/toyproject_housework/LoginActivity.kt
+++ ToyProject_housework/app/src/main/java/com/example/toyproject_housework/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.toyproject_housework
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
@@ -17,25 +19,44 @@ class LoginActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
+
+
         login_register.setOnClickListener {
             startActivity<RegisterActivity>()
         }
-
+        login_login.setOnClickListener {
+            val email = login_id.text.toString()
+            val password = login_passwd.text.toString()
+            signIn(email, password)
+        }
 
     }
 
-    private fun createUser(email: String, passwd: String){
-        auth.createUserWithEmailAndPassword(email,passwd)
-            .addOnCompleteListener { task ->
-                if(task.isSuccessful){
-                    toast("회원가입 성공")
-                    val user = auth.currentUser
-                }else{
-                    toast("회원가입 실패")
-                }
+    @SuppressLint("ResourceAsColor")
+    private fun signIn(email : String, password : String){
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            if(password.length<6){
+                toast("비밀번호는 6자리 이상입니다.")
+            }else{
+                auth?.signInWithEmailAndPassword(email,password)
+                    ?.addOnCompleteListener(this) { task ->
+                        if(task.isSuccessful){
+                            toast("로그인 성공")
+                            moveMainActivity(auth?.currentUser)
+                        }else{
+                            toast("로그인 실패")
+                        }
+                    }
             }
-            .addOnFailureListener{
-                toast("회원가입 실패")
-            }
+        }else{
+            toast("빈칸이 존재합니다.")
+        }
+    }
+
+    fun moveMainActivity(user: FirebaseUser?){
+        if(user != null){
+            startActivity<MainActivity>()
+            login_passwd.setText("")
+        }
     }
 }
--- ToyProject_housework/app/src/main/java/com/example/toyproject_housework/RegisterActivity.kt
+++ ToyProject_housework/app/src/main/java/com/example/toyproject_housework/RegisterActivity.kt
@@ -6,57 +6,84 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_register.*
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_register.*
 import org.jetbrains.anko.toast
 
 
 class RegisterActivity : AppCompatActivity(){
 
-    private lateinit var auth : FirebaseAuth
-    lateinit var family : String
+    private var auth : FirebaseAuth? = null
+    var role = ""
+    var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
 
+        auth = Firebase.auth
+
+        // 스피너 구현
         val item : Array<String> = resources.getStringArray(R.array.family_array)
         val adapter = ArrayAdapter(this,R.layout.spinner_item,item)
         spinner.adapter = adapter
-
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 if(position !=0 ){
-                    family = item[position]
-                    Log.d("TAG",family)
+                    role = item[position]
                 }
             }
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
         }
 
+        register_ok.setOnClickListener {
+            val email = register_id.text.toString()
+            val passwd = register_passwd.text.toString()
+            val name = register_name.text.toString()
 
-
-
+            if(email.isNotEmpty() && passwd.isNotEmpty() && name.isNotEmpty() && role.isNotEmpty()){
+                if(passwd.length<6){
+                    toast("비밀번호는 6자리 이상입니다.")
+                }else{
+                    createUser(email,passwd)
+                }
+            }else{
+                toast("문항을 모두 확인해 주세요.")
+            }
+        }
         // 뒤로가기
         register_cancel.setOnClickListener {
             finish()
         }
     }
-
     private fun createUser(email: String, passwd: String){
-        auth.createUserWithEmailAndPassword(email,passwd)
-            .addOnCompleteListener { task ->
-                if(task.isSuccessful){
-                    toast("회원가입 성공")
-                    val user = auth.currentUser
-                }else{
-                    toast("회원가입 실패")
+            auth?.createUserWithEmailAndPassword(email,passwd)
+                ?.addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        toast("계정 생성 완료")
+                        val user = auth!!.currentUser
+                        val userIDString = user?.uid.toString()
+                        val data = hashMapOf(
+                            "userID" to userIDString,
+                            "name" to register_name.text.toString(),
+                            "role" to role,
+                            "room" to "not yet"
+                        )
+                        db.collection("User")
+                            .add(data)
+                            .addOnSuccessListener {
+                                Log.d("db","파이어스토어 유저 저장 성공")
+                            }
+                            .addOnFailureListener {
+                                Log.d("db","파이어스토어 유저 저장 실패")
+                            }
+                        finish()
+                    }
                 }
-            }
-            .addOnFailureListener{
-                toast("회원가입 실패")
-            }
     }
 }
