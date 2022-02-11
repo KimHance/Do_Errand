29f0688ef702566bc658b6e35c6a9932341cb141
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-02-11 17:20:31 +0900

    다이얼로그 에서 할일 이미지 클릭으로 변경 가능
    fab,애니메이션 사용
    버튼 클릭시 다이얼로그에 잘 적용됨

--- ToyProject_housework/app/src/main/java/com/example/toyproject_housework/CustomDialog.kt
+++ ToyProject_housework/app/src/main/java/com/example/toyproject_housework/CustomDialog.kt
@@ -6,11 +6,15 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.util.Log
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FirebaseFirestore
@@ -19,7 +23,7 @@ import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.dialog_todo.*
 import java.time.LocalDate
 
-class CustomDialog(context: Context) {
+class CustomDialog(context: Context) : View.OnClickListener{
 
     private var auth : FirebaseAuth? = null
     var db = FirebaseFirestore.getInstance()
@@ -28,14 +32,9 @@ class CustomDialog(context: Context) {
     @RequiresApi(Build.VERSION_CODES.O)
     var date : LocalDate = LocalDate.now()
 
+    var isOpen = false
     private val dialog = Dialog(context)
     private lateinit var onClickListener : OnDialogClickListener
-
-//    val user = dialog.findViewById<TextView>(R.id.dialog_userName) // 등록자
-//    val list = dialog.findViewById<TextView>(R.id.dialog_Todo) // 할일
-//    val todoContext = dialog.findViewById<EditText>(R.id.dialog_context) // 내용
-//    val btn_ok = dialog.findViewById<Button>(R.id.dialog_ok) // 완료 버튼
-//    val img = dialog.findViewById<CircleImageView>(R.id.dialog_img) // 이미지
 
     fun setOnClickListener(listener : OnDialogClickListener){
         onClickListener = listener
@@ -46,31 +45,37 @@ class CustomDialog(context: Context) {
         dialog.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.WRAP_CONTENT)
         dialog.setCanceledOnTouchOutside(true)
         dialog.setCancelable(true)
-        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 모서리 삐죽 티어나온거 수정
         dialog.show()
 
         val userID = id
         val roomCode = code
 
+
         val list = dialog.findViewById<TextView>(R.id.dialog_Todo) // 할일
         val user = dialog.findViewById<TextView>(R.id.dialog_userName) // 등록자
         val todoContext = dialog.findViewById<EditText>(R.id.dialog_context) // 내용
-        val btn_cancle = dialog.findViewById<Button>(R.id.dialog_cancle) // 취소 버튼
-        val btn_ok = dialog.findViewById<Button>(R.id.dialog_ok) // 완료 버튼
-        val img = dialog.findViewById<CircleImageView>(R.id.dialog_img) // 이미지
 
-        dialog.dialog_cancle.setOnClickListener {
+        db.collection("User")
+            .document(userID)
+            .get()
+            .addOnSuccessListener {
+                user.text = "${it["name"]}"
+            }.addOnFailureListener {
+
+            }
+
+        dialog.dialog_cancle.setOnClickListener { // 취소버튼
             dialog.dismiss()
         }
 
-        dialog.dialog_ok.setOnClickListener {
+        dialog.dialog_ok.setOnClickListener {  // 완료/추가 버튼
                 db.collection("User")
                     .document(userID)
                     .get()
                     .addOnSuccessListener {
                         Log.d("방코드",roomCode)
                         Log.d("유저 아이디",userID)
-
                         rdb.child(roomCode).child(date.toString()).child(list.text.toString()).child("등록자").setValue(user.text.toString())
                         rdb.child(roomCode).child(date.toString()).child(list.text.toString()).child("내용").setValue(todoContext.text.toString())
                         onClickListener.onClicked(user.text.toString(),list.text.toString(),todoContext.text.toString())
@@ -80,9 +85,170 @@ class CustomDialog(context: Context) {
 //            dialog.dismiss()
         }
 
+
+
+        dialog.dialog_img.setOnClickListener {
+            // 안눌려 있으면
+            if(!isOpen){
+                dialog.floating_cleaning.setOnClickListener(this)
+                dialog.floating_trash.setOnClickListener(this)
+                dialog.floating_shopping.setOnClickListener(this)
+                dialog.floating_pickup.setOnClickListener(this)
+                dialog.floating_laudnry.setOnClickListener(this)
+                dialog.floating_etc.setOnClickListener(this)
+                dialog.floating_cooking.setOnClickListener(this)
+                dialog.floating_clothes.setOnClickListener(this)
+                dialog.floating_dishes.setOnClickListener(this)
+                floatingOpen() // 플로팅 메뉴 열어줌
+                isOpen = true
+                //각 플로팅 버튼 클릭시 이벤트 구현해야함 ;;
+
+            }else{ // 눌려져 있으면
+                dialog.floating_cleaning.setOnClickListener(this)
+                dialog.floating_trash.setOnClickListener(this)
+                dialog.floating_shopping.setOnClickListener(this)
+                dialog.floating_pickup.setOnClickListener(this)
+                dialog.floating_laudnry.setOnClickListener(this)
+                dialog.floating_etc.setOnClickListener(this)
+                dialog.floating_cooking.setOnClickListener(this)
+                dialog.floating_clothes.setOnClickListener(this)
+                dialog.floating_dishes.setOnClickListener(this)
+
+                floatingClose() // 플로팅 메뉴 닫아줌
+                isOpen=false
+            }
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            dialog.floating_cleaning.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.cleaning)
+                dialog.dialog_Todo.text = "청소"
+                isOpen=false
+            }
+            dialog.floating_clothes.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.clothespin)
+                dialog.dialog_Todo.text = "빨래널기&개기"
+                isOpen=false
+            }
+            dialog.floating_cooking.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.cooking)
+                dialog.dialog_Todo.text = "요리"
+                isOpen=false
+            }
+            dialog.floating_dishes.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.apron)
+                dialog.dialog_Todo.text = "설거지"
+                isOpen=false
+            }
+            dialog.floating_etc.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.etc)
+                dialog.dialog_Todo.text = "기타"
+                isOpen=false
+            }
+            dialog.floating_laudnry.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.laundry)
+                dialog.dialog_Todo.text = "빨래"
+                isOpen=false
+            }
+            dialog.floating_pickup.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.pickup)
+                dialog.dialog_Todo.text = "픽업"
+                isOpen=false
+            }
+            dialog.floating_shopping.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.shopping)
+                dialog.dialog_Todo.text = "장보기"
+                isOpen=false
+            }
+            dialog.floating_trash.id ->{
+                floatingClose()
+                dialog.dialog_img.setImageResource(R.drawable.recycle)
+                dialog.dialog_Todo.text = "쓰레기"
+                isOpen=false
+            }
+        }
+    }
+    private fun floatingOpen(){ //눌려져 있으면
+        val fabOpen = AnimationUtils.loadAnimation(dialog.context,R.anim.fab_open)
+        val fabRAntiClockwise = AnimationUtils.loadAnimation(dialog.context,R.anim.rotate_anticlockwise)
+
+        dialog.dialog_img.startAnimation(fabRAntiClockwise)
+        dialog.floating_cleaning.isVisible=true
+        dialog.floating_clothes.isVisible=true
+        dialog.floating_cooking.isVisible=true
+        dialog.floating_dishes.isVisible=true
+        dialog.floating_etc.isVisible=true
+        dialog.floating_laudnry.isVisible=true
+        dialog.floating_pickup.isVisible=true
+        dialog.floating_shopping.isVisible=true
+        dialog.floating_trash.isVisible=true
+
+        //애니메이션 실행(나타남)
+        dialog.floating_cleaning.startAnimation(fabOpen)
+        dialog.floating_clothes.startAnimation(fabOpen)
+        dialog.floating_cooking.startAnimation(fabOpen)
+        dialog.floating_dishes.startAnimation(fabOpen)
+        dialog.floating_etc.startAnimation(fabOpen)
+        dialog.floating_laudnry.startAnimation(fabOpen)
+        dialog.floating_pickup.startAnimation(fabOpen)
+        dialog.floating_shopping.startAnimation(fabOpen)
+        dialog.floating_trash.startAnimation(fabOpen)
+
+        //클릭 가능
+        dialog.floating_cleaning.isClickable=true
+        dialog.floating_clothes.isClickable=true
+        dialog.floating_cooking.isClickable=true
+        dialog.floating_dishes.isClickable=true
+        dialog.floating_etc.isClickable=true
+        dialog.floating_laudnry.isClickable=true
+        dialog.floating_pickup.isClickable=true
+        dialog.floating_shopping.isClickable=true
+        dialog.floating_trash.isClickable=true
+    }
+
+    private fun floatingClose(){
+        val fabClose = AnimationUtils.loadAnimation(dialog.context,R.anim.fab_close)
+        val fabRClockwise = AnimationUtils.loadAnimation(dialog.context,R.anim.rotate_clockwise)
+
+        dialog.dialog_img.startAnimation(fabRClockwise)
+
+        //애니메이션 실행(사라짐)
+        dialog.floating_cleaning.startAnimation(fabClose)
+        dialog.floating_clothes.startAnimation(fabClose)
+        dialog.floating_cooking.startAnimation(fabClose)
+        dialog.floating_dishes.startAnimation(fabClose)
+        dialog.floating_etc.startAnimation(fabClose)
+        dialog.floating_laudnry.startAnimation(fabClose)
+        dialog.floating_pickup.startAnimation(fabClose)
+        dialog.floating_shopping.startAnimation(fabClose)
+        dialog.floating_trash.startAnimation(fabClose)
+
+        //클릭 불가능
+        dialog.floating_cleaning.isClickable=false
+        dialog.floating_clothes.isClickable=false
+        dialog.floating_cooking.isClickable=false
+        dialog.floating_dishes.isClickable=false
+        dialog.floating_etc.isClickable=false
+        dialog.floating_laudnry.isClickable=false
+        dialog.floating_pickup.isClickable=false
+        dialog.floating_shopping.isClickable=false
+        dialog.floating_trash.isClickable=false
+
     }
 
     interface OnDialogClickListener{
         fun onClicked(user : String , todo : String, todoContext: String)
     }
+
 }
